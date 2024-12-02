2ee2f6acb1c03c10d0e8be15e5a63b81552a44a8
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-02 12:06:26 -0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int number = 10;
         String phrase = "Hello world!";
-        System.out.println(phrase);
 
 
     }
