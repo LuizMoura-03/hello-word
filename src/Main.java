9f5ed14ef0b4958be004fc06c2ad4b9470e80a4e
LuizFernando <barbosa.luiz@zup.com.br>
2024-12-02 11:53:43 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 10;
+        String phrase = "Hello world!";
+        System.out.println(phrase);
+
+
+    }
+}
